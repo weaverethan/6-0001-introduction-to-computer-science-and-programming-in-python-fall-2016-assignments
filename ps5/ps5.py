67ac1991124808f21cf4faf0fd1d07a7340c94a4
weaverethan <weaverethan@icloud.com>
2023-09-30 18:36:53 -0500

    class added to ps5

--- ps5/ps5.py
+++ ps5/ps5.py
@@ -56,6 +56,26 @@ def process(url):
 
 # TODO: NewsStory
 
+class NewsStory(object):
+    def __init__(self, guid, title, description, link, pubdate):
+        self.guid = guid
+        self.title = title
+        self.description = description
+        self.link = link
+        self.pubdate = pubdate
+    def get_guid(self):
+        return self.guid
+    def get_title(self):
+        return self.title
+    def get_description(self):
+        return self.description
+    def get_link(self):
+        return self.link
+    def get_pubdate(self):
+        return self.pubdate
+    
+
+
 
 #======================
 # Triggers
