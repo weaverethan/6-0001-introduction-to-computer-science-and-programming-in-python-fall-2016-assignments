eab68d3b8d3f7bd7e6e9585315e511236a590ae6
weaverethan <weaverethan@icloud.com>
2023-10-03 10:21:28 -0500

    hello world in c made

--- 6.S096-Introduction-To-C-And-C++-January-2013/assn1/fibonacci.c
+++ 6.S096-Introduction-To-C-And-C++-January-2013/assn1/fibonacci.c
@@ -1,15 +1,23 @@
 #include <stdio.h>
 
+// Function prototype for fib
+int fib(int n);
+
 void print_fib(int n) {
-	printf("%d\n", fib(n));
+    printf("%d\n", fib(n));
 }
 
 int fib(int n) {
-	if (n <= 0) {
-		return 0;
-	} else if(n == 1) {
-		return 1;
-	} else {
-		return fib(n-1) + fib(n-2);
-	}
+    if (n <= 0) {
+        return 0;
+    } else if(n == 1) {
+        return 1;
+    } else {
+        return fib(n-1) + fib(n-2);
+    }
+}
+
+int main() {
+    // You may want to call the functions from here
+    return 0;
 }
--- 6.S096-Introduction-To-C-And-C++-January-2013/assn1/reverse.c
+++ 6.S096-Introduction-To-C-And-C++-January-2013/assn1/reverse.c
@@ -1,20 +1,46 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 void backwards(char *str, int len) {
-	char reversed[len+1];
-	int pos = 0;
-	int caret = len;
-	reversed[len] = '\0';
-	for (int i = len - 1; i >= -1; --i){
-		if (i == -1 || str[i] == ' '){
-			for (int j = i + 1; j < caret; ++j){
-				reversed[pos++] = str[j];
-			}
-			if (i != -1) {
-				reversed[pos++] = ' ';
-			}
-			caret = i;
-		}
-	}
-	printf("%s\n", reversed);
+    // Allocate memory for reversed string
+    char *reversed = malloc(len + 1);
+
+    if (reversed == NULL) {
+        fprintf(stderr, "Memory allocation failed\n");
+        return;
+    }
+
+    int pos = 0;
+    int caret = len;
+
+    // Iterate through the string to reverse words
+    for (int i = len - 1; i >= 0; --i) {
+        if (i == -1 || str[i] == ' ') {
+            for (int j = i + 1; j < caret; ++j) {
+                reversed[pos++] = str[j];
+            }
+            if (i != -1) {
+                reversed[pos++] = ' ';
+            }
+            caret = i;
+        }
+    }
+
+    // Null-terminate the reversed string
+    reversed[pos] = '\0';
+
+    // Print the reversed string
+    printf("%s\n", reversed);
+
+    // Free the allocated memory
+    free(reversed);
+}
+
+int main() {
+    // Example usage
+    char input[] = "Hello World!";
+    int length = sizeof(input) - 1; // Length excluding the null terminator
+    backwards(input, length);
+
+    return 0;
 }
