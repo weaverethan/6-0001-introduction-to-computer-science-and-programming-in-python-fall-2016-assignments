2a2e4dd22ef29d7bea53319beb10c637396ae9ce
weaverethan <weaverethan@icloud.com>
2023-10-04 10:09:10 -0500

    update

--- 6.S096-Introduction-To-C-And-C++-January-2013/Play Ground/Hello_world.cpp
+++ 6.S096-Introduction-To-C-And-C++-January-2013/Play Ground/Hello_world.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 
-int main() {
-    std::cout << "Hello World" << std::endl;
-    return 0;
+int main()
+{
+	std::cout << sizeof(int) << '\n'; // print how many bytes of memory an int value takes
+
+	return 0;
 }
