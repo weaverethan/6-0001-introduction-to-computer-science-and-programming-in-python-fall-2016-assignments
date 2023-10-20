5d8174497055c0148c2135e32fd1a09c95a6d397
weaverethan <weaverethan@icloud.com>
2023-10-19 20:15:23 -0500

    chapter 6 done

---
+++ 6.S096-Introduction-To-C-And-C++-January-2013/LEARNCPP/Chapter_6/6.7_Q2.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+bool isEven(int x)
+{
+    
+    return !(x % 2);
+}
+
+int main()
+{
+    std::cout << "Enter an integer: ";
+    int x{};
+    std::cin >> x;
+
+    if (isEven(x))
+        std::cout << x << " is even\n";
+    else
+        std::cout << x << " is odd\n";
+
+    return 0;
+}
