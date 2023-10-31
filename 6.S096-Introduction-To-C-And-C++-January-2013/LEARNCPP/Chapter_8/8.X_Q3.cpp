961b54f7dfde6efda0d143c730a82726cc2cdda6
weaverethan <weaverethan@icloud.com>
2023-10-31 08:43:58 -0500

    chapter 8 quiz

--- 6.S096-Introduction-To-C-And-C++-January-2013/LEARNCPP/Chapter_8/8.X_Q3.cpp
+++ 6.S096-Introduction-To-C-And-C++-January-2013/LEARNCPP/Chapter_8/8.X_Q3.cpp
@@ -12,17 +12,41 @@ int getRandomNumber(int min, int max) {
 }
 
 
-int getGuess(){
-	static int guessnum{1};
-	int guess{};
-	std::cout << "Guess #" << guessnum << ":";
-	std::cin >> guess;
-	++guessnum;
-	return guess;
+int getUserGuess() {
+    static int guessNum{1};
+    int userGuess;
+    bool isValid = false;
 
+    while (!isValid) {
+        std::cout << "Guess #" << guessNum << ":";
+        if (std::cin >> userGuess) {
+            if (userGuess >= 1 && userGuess <= 100) {
+                // Check if the input is an integer (no fractional part)
+                if (std::cin.peek() == '\n') {
+                    isValid = true;
+                } else {
+                    std::cout << "Please enter an integer, not a floating-point number." << std::endl;
+                    std::cin.clear();  // Clear error flags
+                    std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');  // Discard invalid input
+                }
+            } else {
+                std::cout << "Please enter a number between 1 and 100." << std::endl;
+                std::cin.clear();  // Clear error flags
+                std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');  // Discard invalid input
+            }
+        } else {
+            std::cout << "Invalid input. Please enter a number." << std::endl;
+            std::cin.clear();  // Clear error flags
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');  // Discard invalid input
+        }
+    }
+
+    return userGuess;
 }
 
-		
+
+
+
 int main(){
 	
 	int secret_number{getRandomNumber(1, 100)};	
@@ -30,15 +54,18 @@ int main(){
 	std::cout << "You have 7 tries to guess what it is.\n";
 	
 	for (int i{1}; i<=7; ++i){
-		if (getGuess() == secret_number){
+		int guess{getUserGuess()};
+		if (guess == secret_number){
 			std::cout << "Correct! You win!\n";
 			return 0;
+		} else {
+			std::cout << "Nope\n";
 		} 
 
 	}	
 
 	std::cout << "no more guess's you loose\n";
-
+	std::cout << "the number was " << secret_number << "\n";
 
 	return 0;
 }
